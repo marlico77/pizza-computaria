c0b79e5cd9cb78df556e45db215d07b7a106a14d
marlico77 <marlonssoficial@gmail.com>
2025-03-18 23:03:37 -0300

    atualização de horario

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkRestaurantOpen() {
   const data = new Date();
   const hora = data.getHours();
-  return hora >= 9 && hora < 23;
+  return hora >= 7 && hora < 25;
   //true = restaurante está aberto 
 }
 
